892153ad62f63cbec94e9e4ff2aeecd1d52c4eac
Raphael S. Andaya <raphyand@gmail.com>
2021-06-13 21:32:19 -0700

    Added dice instead to Game Manger, because only a rolling function seems to be needed.  Added a dice.py just in case.  Added Game Manager initial testing with its class, in pig.py's main function.

--- pig.py
+++ pig.py
@@ -9,8 +9,15 @@
 #   Score Board
 #   Display Manager
 
+import dice
+from GameManager import GameManager
+
 def main():
     print("This is a test.  It should be printed out.")
+    myGameManager = GameManager(0,0)
+    myGameManager.rollDice()
+
+
 if __name__ == "__main__":
     # execute only if run as a script
     main()
